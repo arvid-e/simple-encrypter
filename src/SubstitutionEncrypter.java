b3b8e46f6b394d29e8675751f1f5e8072d4e424e
arvid-e <arvid.edvinsson@gmail.com>
2026-01-30 16:11:56 +0900

    Add comments

--- src/SubstitutionEncrypter.java
+++ src/SubstitutionEncrypter.java
@@ -1,4 +1,12 @@
+/**
+ * Class that performs simple substitution encryption and decryption 
+ * using the ASCII table and a key up to the value 255 (8-bit).
+ */
 public class SubstitutionEncrypter {
+
+    /**
+     * Method for encrypting a message by shifting ASCII values to the right by the key amount.
+     */
     public String encrypt(String message, int key) {
 
         if (key > 255) {
@@ -12,19 +20,21 @@ public class SubstitutionEncrypter {
             int asciiValue = (int) letter;
             int shiftedAsciiValue = asciiValue + key;
 
+            // Handle wrap-around if value exceeds 255
             if (shiftedAsciiValue > 255) {
                 shiftedAsciiValue -= 256;   
             }
 
             char encryptedLetter = (char) shiftedAsciiValue;
-
             encryptedResult.append(encryptedLetter);
         }
-
 
         return encryptedResult.toString();
     }
 
+    /**
+     * Method for decrypting a cipher by shifting the ASCII value to the left by the key amount.
+     */
     public String decrypt(String cipher, int key) {
 
         if (key > 255) {
@@ -38,15 +48,14 @@ public class SubstitutionEncrypter {
             int asciiValue = (int) letter;
             int shiftedAsciiValue = asciiValue - key;
 
+            // Handle wrap around if the ASCII value is lower than 0
             if (shiftedAsciiValue < 0) {
                 shiftedAsciiValue += 256;   
             }
 
             char decryptedLetter = (char) shiftedAsciiValue;
-
             decryptedResult.append(decryptedLetter);
         }
-
 
         return decryptedResult.toString();
     }
